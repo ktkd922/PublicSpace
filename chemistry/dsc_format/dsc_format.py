c02b9a9a33a0b6323b0d90daf17bbc92680b0bd1
ktkd922 <ktkd922@gmail.com>
2022-09-10 14:55:38 +0900

    DSCの単位を重量あたりに変換

--- chemistry/dsc_format/dsc_format.py
+++ chemistry/dsc_format/dsc_format.py
@@ -15,6 +15,9 @@ pyファイルを生データと同じフォルダに入れ、コマンドライ
 python dsc_format.py ***.xlsx
 python dsc_format.py ***.xlsx -p
 
+2022/09/10
+µW/mg単位に変換
+
 '''
 
 import pandas as pd
@@ -27,19 +30,31 @@ df = pd.read_excel(filename)
 
 start_point = 0
 for i in range(100):
-    if df.iloc[i, 0] == 'Time':
+    if df.iloc[i, 0] == 'Sample Weight':
+        sample_weight = df.iloc[i, 1]
+        print('sample weight =', sample_weight, ' mg')
+    elif df.iloc[i, 0] == 'Time':
         start_point = i + 2
         break
 
 data = df.drop(df.index[:start_point])
 data.columns = df.iloc[start_point-2, :]
 data = data.drop(data.columns[4:], axis=1)
+
+# 単位の変換
+data['DSC'] = data['DSC'] / sample_weight
+data['DDSC'] = data['DDSC'] / sample_weight
+data.set_axis(['Time / min', 'DSC / µW mg-1', 'Temp / dC',
+              'DDSC / µW mg-1min-1'], axis='columns', inplace=True)
+
+# 保存
 data.to_csv(filename.removesuffix('.xlsx') + '.csv', index=False)
 
+# pyplot
 if '-p' in args:
     plt.plot(data.iloc[:, 1], data.iloc[:, 2], color='blue')
     plt.xlabel('Temperature / ºC')
-    plt.ylabel('Exotherm / µW')
+    plt.ylabel('Exotherm / µW•mg-1')
     plt.savefig(filename.removesuffix('.xlsx') + '.png',
                 bbox_inches="tight", pad_inches=0.05)
     plt.clf()
